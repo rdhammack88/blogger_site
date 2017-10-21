6f7eea563a9f1c97e64249cab14e72b65cf4eb00
Dustin Hammack <rdhammack88@hotmail.com>
2017-10-21 00:16:46 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,40 @@ $(document).ready(function() {
 		$searchForm.toggleClass('col-sm-4 col-sm-5 col-sm-offset-2 ');
 		$('.input-group .btn').toggleClass('btn-lg');
 	});*/
+	
+	/*-----------------------------------------------------------------------------
+	function inputError( event, $firstError, $secondError, $thirdError ) {
+		
+		if( $firstError.val() === '' ) {
+			event.preventDefault();
+			$('.inputError').show();
+		} else {
+			$('.inputError').hide();
+		}
+		
+		if( $secondError.val() === '' ) {
+			event.preventDefault();
+			$('.inputError').show();
+		} else {
+			$('.inputError').hide();
+		}
+		
+		if( $thirdError.val() === '' ) {
+			event.preventDefault();
+			$('.inputError').show();
+		} else {
+			$('.inputError').hide();
+		}
+	}
+	
+	
+	$('input[name="signup"]').on( "click", inputError( $('#first_name'), $('#email'), $('#password') ) );
+	$('#newBlog').on(inputError( "click", $('#blogTitle'), $('#blogTopic'), $('#blog') ) );
+	$('#saveInfo').on(inputError( "click", $('#first_name'), $('#email'), $('#password') ) );
 
+	*///-------------------------------------------------------------------------------------------
+	
+	
 	$('input[name="signup"]').click(function(e) {
 		var $first_name = $('#first_name');
 		var $email = $('#email');
@@ -27,35 +60,115 @@ $(document).ready(function() {
 		
 		if( $first_name.val() === '' ) {
 			e.preventDefault();
-			$('.inputError').show();
+			$('.nameError').show();
+			$($first_name).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 		} else {
-			$('.inputError').hide();
+			$('.nameError').hide();
+			$($first_name).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
 		}
 		
 		if( $email.val() === '' ) {
 			e.preventDefault();
-			$('.inputError').show();
+			$('.emailError').show();
+			$($email).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 		} else {
-			$('.inputError').hide();
+			$('.emailError').hide();
+			$($email).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
 		}
 		
 		if( $password.val() === '' ) {
 			e.preventDefault();
-			$('.inputError').show();
+			$('.passwordError').show();
+			$($password).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 		} else {
-			$('.inputError').hide();
+			$('.passwordError').hide();
+			$($password).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
 		}
 		
 	});
 	
+	$('input[name="save"]').click(function(e) {
+		var $first_name = $('#first_name_edit');
+		var $email = $('#email_edit');
+		var $newpassword = $('#new_password');
+		
+		if( $first_name.val() === '' ) {
+			e.preventDefault();
+			$('.nameEditError').show();
+			$($first_name).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+		} else {
+			$('.nameEditError').hide();
+			$($first_name).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
+		}
+		
+		if( $email.val() === '' ) {
+			e.preventDefault();
+			$('.emailEditError').show();
+			$($email).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+		} else {
+			$('.emailEditError').hide();
+			$($email).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
+		}
+		
+		/*if( $newpassword.val() ) {
+			e.preventDefault();
+			var $currentPassword 		= $('#current_password');
+			var $currentPasswordRepeat 	= $('#current_password_repeat');
+			
+			if( $currentPassword.val() === '' && $currentPasswordRepeat.val() === '' ) {
+				$('.passwordError, .passwordErrorRepeat').hide();
+				$('.passwordErrorBoth').show();
+				$($currentPassword).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+				$($currentPasswordRepeat).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+			} else if( $currentPassword.val() === '' ) {
+				$('.passwordErrorRepeat, .passwordErrorBoth').hide();
+				$('.passwordError').show();
+				$($currentPassword).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+			} else if( $currentPasswordRepeat.val() === '' ) {
+				$('.passwordError, .passwordErrorBoth').hide();
+				$('.passwordErrorRepeat').show();
+				$($currentPasswordRepeat).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
+			} else {
+				$('.passwordError, .passwordErrorRepeat').hide();
+				$($currentPassword, $currentPasswordRepeat).parent().removeClass('has-error has-feedback').addClass('has-success has-feedback');
+			}
+			
+//			
+//			$('.passwordError').show();
+		} else {
+			$('.passwordError, .passwordErrorRepeat, .passwordErrorBoth').hide();
+		}*/
+		
+	});
+	
+/*	$('button[name="delete"]').click(function(e) {
+//	function delete() {
+		e.stopImmediatePropagation();
+		$confirmDelete = confirm("Are you sure you want to delete your profile account? This cannot be undone!"), e.stopImmediatePropagation();;
+//		"Are you sure you want to delete your profile account? This cannot be undone!"
+//		e.stopImmediatePropagation();
+		//console.log($confirmDelete);
+		console.log(event.eventPhase);
+		//return $confirmDelete;
+		
+		if( !$confirmDelete ) {
+			//console.log($confirmDelete);
+			e.stopPropagation;
+			//e.preventDefault();
+			$.post( "blogs.php");
+		} /*else {
+			e.stopPropagation;
+		}
+		
+	});*/
 	 
-	 $('#newBlog').click(function(e) {
+	$('#newBlog').click(function(e) {
 		var $blog_title = $('#blogTitle');
 		var $blog_topic = $('#blogTopic');
 		var $blog		= $('#blog');
 		var public		= $('input[name="public"]');
 		 		
-		if( !$blog_title.val()) {
+		if( $blog_title.val() === '' ) {
 			e.preventDefault();
 			$($blog_title).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 			$('.titleError').show();
@@ -65,14 +178,14 @@ $(document).ready(function() {
 		}
 		 
 		  /*if( $blog_title.val()) {
-			$blog_title.addClass('has-success, has-feedback');
-			$('.titleError').hide();			
-		}else {
-			e.preventDefault();
-			$('.inputError').show();
-		}*/
+****			$blog_title.addClass('has-success, has-feedback');
+****			$('.titleError').hide();			
+****		}else {
+****			e.preventDefault();
+****			$('.inputError').show();
+****		}*/
 		
-		if( !$blog_topic.val()) {
+		if( $blog_topic.val() === '' ) {
 			e.preventDefault();
 			$($blog_topic).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 			$('.topicError').show();
@@ -81,7 +194,7 @@ $(document).ready(function() {
 			$('.topicError').hide();
 		}
 		
-		if( !$blog.val()) {
+		if( $blog.val() === '' ) {
 			e.preventDefault();
 			$($blog).parent().removeClass('has-success has-feedback').addClass('has-error has-feedback');
 			$('.blogError').show();
@@ -91,10 +204,13 @@ $(document).ready(function() {
 		}
 		 
 		/*if( !public.valueOf ) {
-			public.value = 'public';
-		}*/
+****			public.value = 'public';
+****		}*/
 		
 	});
+	
+	
+	
 	
 	if( $('#mainNav > div').hasClass('col-xs-12') ) {
 		$('#mainNav > div').toggle();
@@ -114,7 +230,7 @@ $(document).ready(function() {
 		});		//.addClass('navbar-fixed-bottom');
 	}*/
 	
-	if( $('body').height() < 600 ) {
+	if( $('body').height() < 550 ) {
 		$('footer').css({ 
 			position: "absolute",
 			bottom: 0,
