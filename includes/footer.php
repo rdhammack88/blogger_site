bc7e57751f38ad2651ed450ca70833c63b01c01f
Dustin Hammack <rdhammack88@hotmail.com>
2018-01-23 00:12:31 -0600

    Finished footer
    footer.php Completed

--- includes/footer.php
+++ includes/footer.php
@@ -1,39 +1,25 @@
-</div> <!-- End of #container -->
+</div> <!-- End of .body-container -->
 
 <!-- Footer -->
-<footer class="text-center col-sm-12 navbar-fixed-bottom"> <!--</footer> navbar-fixed-bottom navbar-inverse">-->
-<!--	<hr>-->
-	<small>Created with <span class="love">&hearts;</span> by <a href="http://www.dustinhammack.com/" target="_blank">Dustin Hammack</a>!</small>  
-
+<footer class="text-center col-sm-12 navbar-fixed-bottom">
+	<small>Created with <span class="love">&hearts;</span> by <a href="http://www.dustinhammack.com/" target="_blank" title="Portfolio site for Dustin Hammack" data-toggle="tooltip" data-placement="bottom">Dustin Hammack</a>!</small>  
 </footer>
 
-
-
-<!-- Scripts -->
+<!-- jQuery Scripts -->
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 
-<!-- Minified JavaScript -->
+<!-- Bootstrap Minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-<!--<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-XXXXXXXX-X', 'auto');
-ga('send', 'pageview');
-</script>-->
-
 
 <!-- Custom Scripts -->
 <script src="js/scripts.js"></script>
 
-<?php 
+<?php
+/* Close connection to DB */
 if(isset($conn)) {
-	mysqli_close($conn); 
+	mysqli_close($conn);
 }
 ?>
-
 
 </body>
 </html>
